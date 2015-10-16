601fc42781f8a710d439f509cda26fc8317ba45e
Rommel Rico <rommeltj@gmail.com>
2015-10-16 10:21:26 -0700

    Initial Commit

---
+++ ScaryBugsMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScaryBugsMac
+//
+//  Created by Rommel Rico on 10/16/15.
+//  Copyright © 2015 Rommel Rico. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScaryBugsMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScaryBugsMac
+//
+//  Created by Rommel Rico on 10/16/15.
+//  Copyright © 2015 Rommel Rico. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
